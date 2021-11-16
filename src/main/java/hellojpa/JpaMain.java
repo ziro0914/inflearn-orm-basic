c00caa46d278853f12e42f1d23c1637a3cb9b886
강태호 <ziro0914@etoos.com>
2021-11-16 13:30:38 +0900

    인프런 강의 자바 ORM 표준 JPA 강의 시작

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory( "hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            //Member findMember =  em.find(Member.class, 2L);
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ src/main/java/hellojpa/Test.java
@@ -0,0 +1,5 @@
+package hellojpa;
+
+public class Test {
+
+}
